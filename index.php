f33b458e91b23eaeff2a6a6fad484335ef6a77a7
Horizonte Junior <hnmagalhaesjr@gmail.com>
2024-07-18 17:22:30 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mesmo Café</title>
+</head>
+<body>
+    <h1>Qual receita vamos fazer hoje?</h1>
+</body>
+</html>
