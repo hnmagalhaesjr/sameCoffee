52bccc8489a22700faa50a0f74d8d0bdd3e0789f
Horizonte Junior <hnmagalhaesjr@gmail.com>
2024-07-18 17:32:35 -0300

    Adicionado formulário

--- index.php
+++ index.php
@@ -5,7 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mesmo Café</title>
 </head>
-<body>
+
     <h1>Qual receita vamos fazer hoje?</h1>
+    <br>
+    <form method="POST" action="preparo.php">
+    <input type="text" name="qtdCafe" placeholder="Digite a quantidade de café"/>
+    <br>
+    <input type="text" name="qtdAgua" placeholder="Digite a quantidade de água"/>
+    </form>
 </body>
 </html>
